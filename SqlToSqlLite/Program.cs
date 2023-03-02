a054413f327220452f01a5a995817ecfbdaa6351
Tomas Sobota <t.sobota@volny.cz>
2023-03-02 19:45:42 +0100

    done, version 1.0

---
+++ SqlToSqlLite/Program.cs
@@ -0,0 +1,91 @@
+﻿using DataDownloader;
+using System;
+
+SpravceDB spravceDB = new SpravceDB();      // spravce databaze
+bool databazeMSOk = false;                  // existují soubory databáze MSSQL
+bool databazeLiteOk = false;                // existují soubory databáze SQLite     
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("MS-SQL TO SQLITE 1.0");
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine("Program načte data ze souboru: Database.mdf a uloží je do Database.sqlite.");
+Console.WriteLine("Funguje jen pro databáze s vytvořenými tabulkami pro projekt ITN Data Downloader.");
+Console.WriteLine("Databáze je potřeba mít ve složce s programem - ve složce Database/");
+
+Console.WriteLine(Directory.GetCurrentDirectory() + "\\Database\\Database.mdf");
+//kontrola existence potřebných souborů souborů
+if (File.Exists(Directory.GetCurrentDirectory() + "\\Database\\Database.mdf"))
+{
+   
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Databáze Database/Database.mdf - existuje");
+    Console.ResetColor();
+    databazeMSOk = true;
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Databáze Database/Database.mdf - neexistuje");
+    Console.ResetColor();
+}
+
+
+if (File.Exists(Directory.GetCurrentDirectory() + "\\Database\\Database.sqlite"))
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Databáze Database/Database.sqlite - existuje");
+    Console.ResetColor();
+    databazeLiteOk = true;
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Databáze Database/Database.sqlite - neexistuje");
+    Console.ResetColor();
+}
+
+// pokud databáze existují
+if(databazeLiteOk && databazeMSOk)
+{
+    Console.WriteLine("Stisknutím jakékoliv klávesy zahájíte kopírování databáze.");
+    Console.ReadKey();
+    Console.WriteLine("Připojuji k MS-SQL databázi a vytahuji data.");
+    try
+    {
+        spravceDB.ZiskejDataMSSql();    // načtení dat z MSSQL
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Nastala chyba při práci s MS-SQL databází: " + ex.Message);
+        Console.ReadKey();
+        Environment.Exit(-1);
+    }
+    Console.WriteLine("Načtení dat z MS-SQL databáze proběhlo v pořádku.");
+
+
+    Console.WriteLine("Připojuji se k SQLite databázi a ukládám data");
+    try
+    {
+        spravceDB.UlozDoSqlite();       //Uložení dat do SQLite
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Nastala chyba při práci s SQLite databází: " + ex.Message);
+        Console.ReadKey();
+        Environment.Exit(-1);
+    }
+    Console.WriteLine("Uložení dat do SQLite databáze proběhlo v pořádku.");
+
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Nakopírujte správné databázové soubory do složky Database a spusťte program znovu!");
+    Console.ResetColor();
+}
+
+Console.ReadKey();
+
---
+++ SqlToSqlLite/SpravceDB.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDownloader
+{
+    internal class SpravceDB
+    {
+        //connectionstring databáze SQLite k projektu ITN Data Presenter
+        private string pripojovaciStringLite;
+
+        //connectionstring databáze MS-SQL u projektu ITN Data Downloader
+        private string pripojovaciString;
+
+        //seznam uzivatelu stahnutý třídou ITNetwork
+        private List<User> seznamITN;
+
+
+        public SpravceDB()
+        {
+            seznamITN = new();
+            string adresar = Directory.GetCurrentDirectory();
+            pripojovaciStringLite = "Data Source=" + adresar + "\\Database\\Database.sqlite;Version=3;";
+            pripojovaciString = "Data Source=(localdb)\\MSSQLLocalDB;AttachDbFilename="+adresar+"\\Database\\Database.mdf;Integrated Security=True";
+                //"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=" + adresar + "\\Database\\Database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        }
+
+
+        /// <summary>
+        /// Uloží načtená data ze seznamu uživatelů do SQLite databáze
+        /// </summary>
+        public void UlozDoSqlite()
+        {
+            using (SQLiteConnection spojeni = new SQLiteConnection(pripojovaciStringLite))
+            {
+
+                spojeni.Open();
+               
+                using (SQLiteCommand prikaz = new SQLiteCommand()) {
+                    prikaz.Connection = spojeni;
+
+                    prikaz.CommandText = "INSERT INTO" +
+                                              " ITN_USERS(Jmeno,UrlObrazek,Urlwww,IdNaITN,Vek,Zkusenost,Aura)" +
+                                              " VALUES " +
+                                              " (@Jmeno,@UrlObrazek,@Urlwww,@IdNaITN,@Vek,@Zkusenost,@Aura)";
+
+                    for (int i = 0; i < seznamITN.Count; i++)
+                    {
+                        prikaz.Parameters.Clear();
+
+                        prikaz.Parameters.AddWithValue("@Jmeno", seznamITN[i].Jmeno);
+                        prikaz.Parameters.AddWithValue("@UrlObrazek", seznamITN[i].ObrazekWWW);
+                        prikaz.Parameters.AddWithValue("@Urlwww", seznamITN[i].Stranka);
+                        prikaz.Parameters.AddWithValue("@IdNaITN", seznamITN[i].Id);
+                        prikaz.Parameters.AddWithValue("@Vek", seznamITN[i].Vek);
+                        prikaz.Parameters.AddWithValue("@Zkusenost", seznamITN[i].Zkusenost);
+                        prikaz.Parameters.AddWithValue("@Aura", seznamITN[i].Aura);
+
+                        prikaz.ExecuteNonQuery(); 
+
+                    }
+
+
+
+                }
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// Načte data z MS-Sql databáze do seznamu uživatelů  - List<User>
+        /// </summary>
+        public void ZiskejDataMSSql()
+        {
+            using (SqlConnection spojeni = new SqlConnection(pripojovaciString))
+            {
+
+                spojeni.Open();
+                string dotaz = "SELECT * FROM ITN_Users";
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(dotaz, spojeni))
+                using (DataSet vysledky = new DataSet())
+                {
+                    adapter.Fill(vysledky);
+
+                    string jmeno;
+                    string obrazekWWW;
+                    string stranka;
+                    int id;
+                    int vek;
+                    int zkusenost;
+                    int aura;
+                    foreach (DataRow item in vysledky.Tables[0].Rows)
+                    {
+
+                        jmeno = item["Jmeno"].ToString();
+                        obrazekWWW = item["UrlObrazek"].ToString();
+                        stranka = item["Urlwww"].ToString();
+                        id = int.Parse(item["IdNaITN"].ToString());
+                        vek = int.Parse(item["Vek"].ToString());
+                        zkusenost = int.Parse(item["Zkusenost"].ToString());
+                        aura = int.Parse(item["Aura"].ToString());
+
+                        seznamITN.Add(new User(jmeno, obrazekWWW, stranka, id, vek, zkusenost, aura));
+                       
+                    }
+
+
+                }
+
+            }
+
+
+        }
+    }
+}
